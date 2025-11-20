54748d1b92de57573659eba80f837c510b81bd24
Rohit Narurkar <rohit.narurkar@proton.me>
2025-11-20 20:41:15 +0000

    chore: update deps and fix build

--- src/verifier/generate.rs
+++ src/verifier/generate.rs
@@ -1,28 +1,26 @@
-use openvm_native_recursion::halo2::utils::{CacheHalo2ParamsReader, Halo2ParamsReader};
-use openvm_sdk::{DefaultStaticVerifierPvHandler, Sdk, config::AggConfig};
-
-/// The default directory to locate openvm's halo2 SRS parameters.
-const DEFAULT_PARAMS_DIR: &str = concat!(env!("HOME"), "/.openvm/params/");
+use openvm_native_recursion::halo2::utils::Halo2ParamsReader;
+use openvm_sdk::Sdk;
 
 /// Generate and return the EVM PLONK verifier's initcode.
 pub(crate) fn generate() -> eyre::Result<Vec<u8>> {
-    let halo2_params_reader = CacheHalo2ParamsReader::new(DEFAULT_PARAMS_DIR);
+    // Get the OpenVM SDK.
+    let sdk = Sdk::riscv32();
 
-    let agg_pk = Sdk::new().agg_keygen(
-        AggConfig::default(),
-        &halo2_params_reader,
-        &DefaultStaticVerifierPvHandler,
-    )?;
+    // Get the standard Halo2 proving key from the SDK.
+    let halo2_pk = sdk.halo2_pk();
 
-    let halo2_params =
-        halo2_params_reader.read_params(agg_pk.halo2_pk.wrapper.pinning.metadata.config_params.k);
+    // Read the Halo2 parameters for the default degree (k).
+    let halo2_params = sdk
+        .halo2_params_reader()
+        .read_params(halo2_pk.wrapper.pinning.metadata.config_params.k);
 
+    // Generate and return the verifier's bytecode.
     Ok(snark_verifier_sdk::evm::gen_evm_verifier_shplonk::<
         snark_verifier_sdk::halo2::aggregation::AggregationCircuit,
     >(
         &halo2_params,
-        agg_pk.halo2_pk.wrapper.pinning.pk.get_vk(),
-        agg_pk.halo2_pk.wrapper.pinning.metadata.num_pvs.clone(),
+        halo2_pk.wrapper.pinning.pk.get_vk(),
+        halo2_pk.wrapper.pinning.metadata.num_pvs.clone(),
         None,
     ))
 }

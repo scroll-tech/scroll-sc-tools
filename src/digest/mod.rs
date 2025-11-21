72cf126966940460cafa377f136f2385b6036ce8
Rohit Narurkar <rohit.narurkar@proton.me>
2025-11-21 10:40:06 +0000

    use v0.7.0 and import commits from scroll-zkvm-verifier

--- src/digest/mod.rs
+++ src/digest/mod.rs
@@ -6,23 +6,12 @@ mod compute;
 #[derive(Debug, Args)]
 pub struct ComputeCommand;
 
-/// Commitment copied from [`v0.7.0-rc.5`][bundle_exe_commit].
-///
-/// [bundle_exe_commit]: https://github.com/scroll-tech/zkvm-prover/blob/v0.7.0-rc.5/crates/circuits/bundle-circuit/bundle_exe_commit.rs
-const EXE_COMMIT: [u32; 8] = [
-    1144920578, 672155974, 440997509, 565686465, 150547879, 714324919, 1665603288, 907252136,
-];
-
-/// Commitment copied from [`v0.7.0-rc.5`][bundle_vm_commit].
-///
-/// [bundle_vm_commit]: https://github.com/scroll-tech/zkvm-prover/blob/v0.7.0-rc.5/crates/circuits/bundle-circuit/bundle_vm_commit.rs
-const VM_COMMIT: [u32; 8] = [
-    702922786, 974900043, 1870917533, 1628966797, 1650497578, 697799835, 298481193, 1937656708,
-];
-
 impl ComputeCommand {
     pub fn run(self) -> eyre::Result<()> {
-        let (exe, leaf) = (EXE_COMMIT, VM_COMMIT);
+        let (exe, leaf) = (
+            scroll_zkvm_verifier::commitments::EXE_COMMIT,
+            scroll_zkvm_verifier::commitments::VM_COMMIT,
+        );
 
         // Fr::to_bytes(&self) spits out little-endian bytes, so we reverse the order to finally
         // display the big-endian bytes in hex-encoded form.

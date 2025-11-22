dbd21a3c409386a8da528747f9c060835883550c
Zhang Zhuo <mycinbrin@gmail.com>
2025-11-22 09:23:07 +0800

    fix scroll-sc-tools generate-verifier --recompute

--- src/verifier/helpers.rs
+++ src/verifier/helpers.rs
@@ -1,4 +1,3 @@
-use openvm_native_recursion::halo2::utils::Halo2ParamsReader;
 use openvm_sdk::Sdk;
 use snark_verifier_sdk::snark_verifier::loader::evm::compile_solidity;
 
@@ -19,23 +18,10 @@ pub(crate) fn generate() -> eyre::Result<Vec<u8>> {
     // Get the OpenVM SDK.
     let sdk = Sdk::riscv32();
 
-    // Get the standard Halo2 proving key from the SDK.
-    let halo2_pk = sdk.halo2_pk();
-
-    // Read the Halo2 parameters for the default degree (k).
-    let halo2_params = sdk
-        .halo2_params_reader()
-        .read_params(halo2_pk.wrapper.pinning.metadata.config_params.k);
-
     // Generate and return the verifier's bytecode.
-    Ok(snark_verifier_sdk::evm::gen_evm_verifier_shplonk::<
-        snark_verifier_sdk::halo2::aggregation::AggregationCircuit,
-    >(
-        &halo2_params,
-        halo2_pk.wrapper.pinning.pk.get_vk(),
-        halo2_pk.wrapper.pinning.metadata.num_pvs.clone(),
-        None,
-    ))
+    let sol_code = sdk.generate_halo2_verifier_solidity()?.halo2_verifier_code;
+
+    Ok(compile_solidity(&sol_code))
 }
 
 /// Download the published Halo2 verifier from GitHub and compile to binary.
